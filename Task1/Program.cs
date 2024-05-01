f862fa5cca346c8b17499f6a00907cb6fafc9d18
Aleksandra PLatanaya <platanaya48@gmail.com>
2024-05-01 18:52:54 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 1
+
+/* Задайте массив.Напишите программу,которая определяет,присутствует ли заданное число в массиве. 
+Программа должнавыдать ответ:Да/Нет.
+Примеры
+[1 3 4 19 3], 8 => Нет
+[-4 3 4 1], 3 => Да */
+
+int[] array01 = { 1, 3, 4, 19, 3 };
+// int[] array02 = {-4,3,4,1};
+
+Console.WriteLine("Введите искомое число в массиве: ");
+int searchVar = Convert.ToInt32(Console.ReadLine());
+bool isExist = false;
+
+
+for(int i = 0; i < array01.Length; i++)
+{
+    if(array01[i] == searchVar)
+    {
+        isExist = true;
+        break;
+    }
+}
+string result = isExist ? "Да" : "Нет"; //тернарный оператор!!!
+Console.WriteLine(result);
+
+// можно было так Console.WriteLine(isExist ? "Да" : "Нет"); было бы то же самое
