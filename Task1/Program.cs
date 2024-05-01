8cb60a479a262a07c389b90604940997f6ced8a5
Aleksandra PLatanaya <platanaya48@gmail.com>
2024-05-01 22:12:57 +0300

    Task1_1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,28 +1,50 @@
 ﻿//Задача 1
 
-/* Задайте массив.Напишите программу,которая определяет,присутствует ли заданное число в массиве. 
-Программа должнавыдать ответ:Да/Нет.
-Примеры
-[1 3 4 19 3], 8 => Нет
-[-4 3 4 1], 3 => Да */
+// Задайте массив.Напишите программу,которая определяет,присутствует ли заданное число в массиве. 
+// Программа долж навыдать ответ:Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да 
 
-int[] array01 = { 1, 3, 4, 19, 3 };
-// int[] array02 = {-4,3,4,1};
+// int[] array01 = { 1, 3, 4, 19, 3 };
+// // int[] array02 = {-4,3,4,1};
 
-Console.WriteLine("Введите искомое число в массиве: ");
-int searchVar = Convert.ToInt32(Console.ReadLine());
-bool isExist = false;
+// Console.WriteLine("Введите искомое число в массиве: ");
+// int searchVar = Convert.ToInt32(Console.ReadLine());
+// bool isExist = false;
 
 
-for(int i = 0; i < array01.Length; i++)
+// for(int i = 0; i < array01.Length; i++)
+// {
+//     if(array01[i] == searchVar)
+//     {
+//         isExist = true;
+//         break;
+//     }
+// }
+// string result = isExist ? "Да" : "Нет"; //тернарный оператор!!!
+// Console.WriteLine(result);
+
+// можно было так Console.WriteLine(isExist ? "Да" : "Нет"); было бы то же самое
+
+
+// int[] array1=new int[5];
+// int[] array2=new int[5] {3,6,8,3,9};
+// int[] array3=new int[] {3,6,8,3,9};
+
+int[] array = { 1, 3, 8, 4, 19, 3, 8 };
+int num = 8;
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
 {
-    if(array01[i] == searchVar)
+    if (array[i] == num)
     {
-        isExist = true;
+        isExistNum = true;
         break;
     }
 }
-string result = isExist ? "Да" : "Нет"; //тернарный оператор!!!
-Console.WriteLine(result);
 
-// можно было так Console.WriteLine(isExist ? "Да" : "Нет"); было бы то же самое
+//string result = isExistNum ? "Да" : "Нет"; 
+Console.WriteLine(isExistNum ? "Да" : "Нет");
--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dacf8f2091b460173d59c67df610011d902ebd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
